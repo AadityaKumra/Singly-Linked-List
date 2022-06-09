30d8ba4c5b2c8e5e8f5b1fb1320b9ca9faefcad9
Aaditya Kumra <72290209+AadityaKumra@users.noreply.github.com>
2022-06-09 10:51:56 +0000

    added print statements of all the three insertion case examples.

--- Main.java
+++ Main.java
@@ -1,9 +1,12 @@
 class Main {
   public static void main(String[] args) {
-    Singlylinkedlist sLL=new Singlylinkedlist();
+    Singlylinkedlist sLL = new Singlylinkedlist();
     sLL.createSinglyLinkedList(5);
-    
-    sLL.insertInLinkedList(6,0);
-    System.out.println(sLL.head.value);  
+
+    sLL.insertInLinkedList(6, 1);
+    sLL.insertInLinkedList(7,3);
+    System.out.println(sLL.head.value);
+    System.out.println(sLL.head.next.value);
+    System.out.println(sLL.head.next.next.value);
   }
-}
+}   
